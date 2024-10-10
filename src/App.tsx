7fb393ec0b6cec894abdf2c5715ed624cbe03d60
IannisG10 <guerraiannis@gmail.com>
2024-10-10 14:23:14 +0200

    Affichage du menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import InputField from './components/InputField/InputField';
 import TaskList from './components/TaskList/TaskList';
+import Menue from './components/Menu/Menu';
 import { TaskProvider } from './hook/TaskContext';
 
 
@@ -12,7 +13,10 @@ const App: React.FC = ()=> {
 
     return(
       <div className='flex flex-col justify-center items-center gap-7 mx-5 my-5 py-3 
-            rounded-md border border-gray-200 shadow-md '>
+            rounded-md border border-gray-200 shadow-md relative'>
+        <div className='absolute left-0 top-1 mx-1'>
+          <Menue/>
+        </div>
         <SearchBar/>
         <TaskProvider>
             <InputField/>
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,8 +4,8 @@ import { Menu } from "lucide-react";
 
 const Menue: React.FC = ()=> {
     return(
-        <div>
-            <Menu/>
+        <div className=' p-1 cursor-pointer border border-gray-200 rounded-md shadow hover:bg-gray-200 transition ease-in-out'>
+            <Menu size={24}/>
         </div>
     )
 }
