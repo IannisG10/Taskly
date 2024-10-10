bcfa3aac3f216b40321e391b853da5d75b608bf8
IannisG10 <guerraiannis@gmail.com>
2024-10-10 14:07:08 +0200

    Add a menu field

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import InputField from './components/InputField/InputField';
 import TaskList from './components/TaskList/TaskList';
 import { TaskProvider } from './hook/TaskContext';
 
+
 const App: React.FC = ()=> {
 
 
 
     return(
-      <div className=' flex flex-col justify-center items-center gap-7 mx-5 my-5 py-3 
+      <div className='flex flex-col justify-center items-center gap-7 mx-5 my-5 py-3 
             rounded-md border border-gray-200 shadow-md '>
         <SearchBar/>
         <TaskProvider>
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Menu } from "lucide-react";
+
+
+const Menue: React.FC = ()=> {
+    return(
+        <div>
+            <Menu/>
+        </div>
+    )
+}
+
+export default Menue;
