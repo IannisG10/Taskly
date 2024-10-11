439f4ddf9311ce2be5362b09cdfea6c646620354
IannisG10 <guerraiannis@gmail.com>
2024-10-11 09:38:57 +0200

    Ajout des icones

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -16,7 +16,7 @@ const Menue: React.FC<MenueProps> = ({openMenue,setOpenMenue})=> {
 
     return(
         
-       <div className={`${openMenue && "shadow-none border-white"} p-1 border border-gray-200 rounded-md cursor-pointer absolute top-0 left-0 mx-5 my-1 
+       <div className={`${openMenue && "shadow-none border-white"} p-1 border border-gray-200 rounded-md cursor-pointer fixed top-8 left-5 mx-5 my-1 
        hover:bg-gray-200 hover:shadow-none shadow ease-in-out duration-150 z-50`}
             onClick={handleOpenMenue}
        >
--- src/components/Menu/sideBar.tsx
+++ src/components/Menu/sideBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CalendarDays,ListTodo,Trash2,Star } from "lucide-react";
 
 interface sideBarProps { 
     openMenue: boolean
@@ -9,9 +10,33 @@ const SideBar: React.FC<sideBarProps> = ({openMenue}) => {
         <div className={`${openMenue && "translate-x-1 transition shadow-md my-4 rounded ease-in-out duration-800 opacity-100"} h-screen flex justify-center shadow-s
                 absolute top-0 left-3 w-1/5  transition duration-500 -translate-x-96 z-0 `}>
 
-            <div className='flex justify-center border border-gray-100 rounded my-8 
-                shadow-md h-1/6 w-2/3 font-bold font-josefin text-xl p-2'>
-                Menu
+            <div className='flex flex-col justify-center gap-2 border border-gray-100 rounded my-8 
+                shadow-md w-2/3 h-1/3 px-3 py-2'>
+                <div className='font-bold font-josefin text-xl'>
+                    Menu
+                </div>
+
+                <div className="flex items-center gap-1">
+                    <div>
+                        <ListTodo size={21}/>
+                    </div>
+                    <h3 className='text-sm font-semibold'>Toutes les Taches</h3>
+                </div>
+
+                <div className='flex items-center gap-1 ml-5'>
+                    <CalendarDays size={21}/>
+                    <h3 className='text-sm font-semibold'>Aujourd'hui</h3>
+                </div>
+
+                <div className='flex items-center gap-1 ml-5'>
+                    <Star size={21}/>
+                    <h3 className='text-sm font-semibold'>Important</h3>
+                </div>
+
+                <div className='flex items-center gap-1 ml-5'>
+                    <Trash2 size={21}/>
+                    <h3 className='text-sm font-semibold'>Corbeille</h3>
+                </div>
             </div>
         </div>
     );
