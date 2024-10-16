a0d05d9c8fedc99cd37c4883bd4171d9db14f4e7
IannisG10 <guerraiannis@gmail.com>
2024-10-16 11:04:00 +0200

    style de la corbeille

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -9,48 +9,6 @@ const TaskList: React.FC = () => {
 
     return(
         <div className='flex flex-col gap-2 w-1/4 relative'>
-            {/* {task.length > 0 ?
-                task.map((item,index) => (
-                    <div key={index}
-                         className='flex flex-col relative border px-2 py-2 w-full border-gray-200 rounded-md shadow gap-1'
-                    >
-                        
-                        <div className='flex justify-start items-center'>
-                            <div>
-                                <Checkbox/>
-                            </div>
-                            <div className='inline-block font-medium text-base ml-1 w-auto'>
-                                {item.description}
-                            </div>
-                        </div>
-
-                        <div className='flex flex-row items-center gap-1'>
-                            {item.TagList.map((itemTag,index) => (
-                                <div className='w-fit text-xs bg-gray-400 py-1 px-2 rounded-lg font-medium'
-                                        key={index}
-                                >
-                                    {itemTag}
-                                </div>
-                            ))}
-                        </div>
-
-                        <div className='flex flex-row items-center gap-2 cursor-pointer'>
-                            <div className='flex justify-center items-center gap-1 border border-gray-200 
-                               font-semibold px-2 py-1 rounded-md hover:bg-gray-200 hover:border-gray-300 ease-in-out'>
-                                <Clock3 size={20}/>
-                                <div>{item.date}</div>
-                            </div>
-                            
-                            <div className='p-2 hover:bg-gray-200 cursor-pointer rounded-md'
-                                 onClick={()=>{deleteTask(item.id)}}
-                            >
-                                <Trash2 size={20}/>
-                            </div>
-                        </div>
-                        
-                    </div>
-                )) : <div className='font-josefin font-semibold text-gray-400'>Aucune Taches</div>
-            } */}
 
             {taskNotFound ? reserchTask.map((item,index) =>(
                 <div key={index}
--- src/components/TrashList/TrashList.tsx
+++ src/components/TrashList/TrashList.tsx
@@ -3,6 +3,8 @@ import { useTask } from "@/hook/TaskContext";
 import { Checkbox } from "../ui/checkbox";
 import SideBar from "../Menu/sideBar";
 import Menue from "../Menu/Menu";
+import { Clock3 } from "lucide-react";
+import { ArchiveRestore } from "lucide-react";
 
 const TrashList: React.FC = ()=> {
     const { trashedTask } = useTask();
@@ -18,23 +20,46 @@ const TrashList: React.FC = ()=> {
                        setOpenMenue={setIsOpen}
                 />
                 <SideBar openMenue={isOpen}/>
-                <div className='flex flex-col justify-center items-center'>
+                <div className='flex flex-col justify-center items-center w-1/4'>
                     <div className='my-2'>
                         <h1 className='text-3xl font-bold font-josefin italic '>Taches supprimés</h1>
                     </div>
                     {trashedTask.length > 0 ?
                         trashedTask.map((item,index) => (
                             <div key={index}
-                                 className='flex flex-col'
+                                 className='flex flex-col gap-1 border-gray-100 border rounded-md shadow-md px-2 py-2 w-full'
                             >
-                                 <div className='flex'>
+                                <div className='flex items-center'>
                                     <div>
                                         <Checkbox/>
                                     </div>
-                                    <div>
+                                    <div className='font-medium text-base ml-1 w-auto inline-block'>
                                         {item.description}
                                     </div>
-                                 </div>
+                                </div>
+
+                                <div className='flex flex-row gap-1 items-center'>
+                                    {item.TagList.map((itemTag,index) => (
+                                        <div key={index}
+                                             className='w-fit text-xs bg-gray-400 py-1 px-2 font-medium rounded-lg'
+                                        >
+                                            {itemTag}
+                                        </div>
+                                    ))}
+                                </div>
+
+                                <div className='flex flex-row items-center gap-2 cursor-pointer'>
+                                    <div className='flex justify-center items-center gap-1 
+                                            border-gray-200 border font-semibold px-2 py-1 rounded-md hover:bg-gray-200 hover:border-gray-300 ease-in-out'>
+                                        <Clock3 size={20}/>
+                                        <div>
+                                            {item.date}
+                                        </div>
+                                    </div>
+                                    <div className='p-2 hover:bg-gray-200 cursor-pointer rounded-md'>
+                                        <ArchiveRestore size={20}/>
+                                    </div>
+                                </div>
                             </div>
                         )) : 
                             <div className='font-josefin font-semibold text-gray-400'> 
