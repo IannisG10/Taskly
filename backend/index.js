d33a3dedad39a86bb3ef8d64f003a6119f4abf33
IannisG10 <guerraiannis@gmail.com>
2024-11-09 13:27:35 +0100

    Configuration de la base de données

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const mongoose = require("mongoose") 
 require("dotenv").config(); //Charge les variablees d'environnement 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb+srv://guerraiannis:<db_password>@cluster0.cs7fn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then("Connecté à la base de donnée")
+.catch("Erreur de connexion à la base de donnée")
 
 app.get("/",(req,res) => {
     res.send("Bienvenue sur mon serveur Express sur render ")
