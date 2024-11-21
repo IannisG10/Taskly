5b83701c5af7ff2499f5efadb905c39246879356
IannisG10 <guerraiannis@gmail.com>
2024-11-21 07:31:42 +0100

    new connection string

--- backend/model/task.js
+++ backend/model/task.js
@@ -4,6 +4,6 @@ const taskSchema = new mongoose.Schema({
     description: String
 })
 
-const Task = mongoose.model("Task",taskSchema)
+const Task = mongoose.model("newTasks",taskSchema)
 
 module.exports = Task
